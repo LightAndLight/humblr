8a0d92b7684effbd23dfbd685471e8b57c4f9377
Isaac Elliott <isaace71295@gmail.com>
2016-08-14 16:53:32 +1000

    Added database schema

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id serial primary key,
+    username varchar(15),
+    email varchar(100),
+    password_hash bytea,
+    salt bytea
+);
+
+CREATE TABLE posts (
+    id serial primary key,
+    user_id integer references users(id),
+    title varchar(100),
+    body varchar(10000)
+);
